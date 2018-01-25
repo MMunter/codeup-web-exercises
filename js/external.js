9e4413307c90d03904ba9b5a0fe4cbebbfe5968b
mdm911 <mdm911@yahoo.com>
2018-01-25 16:00:49 -0600

    Added toLowerCase to color variable

--- js/external.js
+++ js/external.js
@@ -1,4 +1,4 @@
 console.log("Hello from external JavaScript.");
 alert("Welcome to my Website!");
 var color = prompt("What is your favorite color?");
-alert("Great," + " " + color + " " + "is my favorite color, too!");
+alert("Great," + " " + color.toLowerCase() + " " + "is my favorite color, too!");
