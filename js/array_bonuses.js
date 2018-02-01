3a95462951ce135abc20e2a6a5992a75b012f3b3
mdm911 <mdm911@yahoo.com>
2018-02-01 10:06:44 -0600

    Completed planets-array.js exercise

---
+++ js/array_bonuses.js
@@ -0,0 +1,29 @@
+function average(numbers) {
+    var total = 0;
+    numbers.forEach(function (number) {
+        total += number;
+    });
+    return total / numbers.length;
+}
+
+var array = [1,2,3];
+console.log(average(array));
+var test = [4,5,6,9];
+console.log(average(test));
+
+function sumAll(numbers){
+    var total = 0;
+    numbers.forEach(function(number){
+       total += number;
+    });
+    return total;
+}
+
+console.log(sumAll(array));
+console.log(sumAll(test));
+
+function max(numbers){
+    numbers.forEach(function(number){
+       ma
+    });
+}
