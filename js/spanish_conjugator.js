5912f2c19138f81009fa06efd55220018189bf74
mdm911 <mdm911@yahoo.com>
2018-02-03 23:42:26 -0600

    Added accents to pronouns and verb endings, also added document.write command

--- js/spanish_conjugator.js
+++ js/spanish_conjugator.js
@@ -2,34 +2,34 @@
 function conjugateErPresentTense (string) {
     var yo = "Yo " + string + "o\n";
     var nos = "Nosotros " + string +"emos\n";
-    var tu = "Tu " + string + "es\n";
+    var tu = "Tú " + string + "es\n";
     var ud = "Usted " + string + "e\n";
     var uds = "Ustedes " + string + "en";
     return yo + nos + tu + ud + uds;
 }
 
 function conjugateErPastTense (string) {
-    var yo = "Yo " + string + "i\n";
+    var yo = "Yo " + string + "í\n";
     var nos = "Nosotros " + string + "imos\n";
-    var tu = "Tu " + string + "iste\n";
-    var ud = "Usted " + string + "io\n";
+    var tu = "Tú " + string + "iste\n";
+    var ud = "Usted " + string + "ió\n";
     var uds = "Ustedes " + string + "ieron";
     return yo + nos + tu + ud + uds;
 }
 
 function conjugateErFutureTense (string) {
-    var yo = "Yo " + string + "ere\n";
+    var yo = "Yo " + string + "eré\n";
     var nos = "Nosotros " + string + "eremos\n";
-    var tu = "Tu " + string + "eras\n";
-    var ud = "Usted " + string + "era\n";
-    var uds = "Ustedes " + string + "eran";
+    var tu = "Tú " + string + "erás\n";
+    var ud = "Usted " + string + "erá\n";
+    var uds = "Ustedes " + string + "erán";
     return yo + nos + tu + ud + uds;
 }
 
 function conjugateArPresentTense (string) {
     var yo = "Yo " + string + "o\n";
     var nos = "Nosotros " + string + "amos\n";
-    var tu = "Tu " + string + "as\n";
+    var tu = "Tú " + string + "as\n";
     var ud = "Usted " + string + "a\n";
     var uds = "Ustedes " + string + "an";
     return yo + nos + tu + ud + uds;
@@ -38,7 +38,7 @@ function conjugateArPresentTense (string) {
 function conjugateArPastTense (string) {
     var yo = "Yo " + string + "e\n";
     var nos = "Nosotros " + string + "amos\n";
-    var tu = "Tu " + string + "aste\n";
+    var tu = "Tú " + string + "aste\n";
     var ud = "Usted " + string + "o\n";
     var uds = "Ustedes " + string + "aron";
     return yo + nos + tu + ud + uds;
@@ -47,7 +47,7 @@ function conjugateArPastTense (string) {
 function conjugateArFutureTense (string) {
     var yo = "Yo " + string + "are\n";
     var nos = "Nosotros " + string + "aremos\n";
-    var tu = "Tu " + string + "aras\n";
+    var tu = "Tú " + string + "aras\n";
     var ud = "Usted " + string + "ara\n";
     var uds = "Ustedes " + string + "aran";
     return yo + nos + tu + ud + uds;
@@ -56,27 +56,27 @@ function conjugateArFutureTense (string) {
 function conjugateIrPresentTense (string) {
     var yo = "Yo " + string + "o\n";
     var nos = "Nosotros " + string + "imos\n";
-    var tu = "Tu " + string + "es\n";
+    var tu = "Tú " + string + "es\n";
     var ud = "Usted " + string + "e\n";
     var uds = "Ustedes " + string + "en";
     return yo + nos + tu + ud + uds;
 }
 
 function conjugateIrPastTense (string) {
-    var yo = "Yo " + string + "i\n";
+    var yo = "Yo " + string + "í\n";
     var nos = "Nosotros " + string + "imos\n";
-    var tu = "Tu " + string + "iste\n";
-    var ud = "Usted " + string + "io\n";
+    var tu = "Tú " + string + "iste\n";
+    var ud = "Usted " + string + "ió\n";
     var uds = "Ustedes " + string + "ieron";
     return yo + nos + tu + ud + uds;
 }
 
 function conjugateIrFutureTense (string) {
-    var yo = "Yo " + string + "ire\n";
+    var yo = "Yo " + string + "iré\n";
     var nos = "Nosotros " + string + "iremos\n";
-    var tu = "Tu " + string + "iras\n";
-    var ud = "Usted " + string + "ira\n";
-    var uds = "Ustedes " + string + "iran";
+    var tu = "Tú " + string + "irás\n";
+    var ud = "Usted " + string + "irá\n";
+    var uds = "Ustedes " + string + "irán";
     return yo + nos + tu + ud + uds;
 }
 
@@ -111,37 +111,37 @@ while(tense != "past" && tense != "present" && tense != "future") {
 //tests to see which verb ending & tense is needed
 if (verbErTest(verb) && (tense == "past")) {
     verb = verb.substring(0, verb.length - 2);
-    alert(conjugateErPastTense(verb));
+    document.write(conjugateErPastTense(verb));
 }
 else if (verbErTest(verb) && (tense =="present")) {
     verb = verb.substring(0, verb.length - 2);
-    alert(conjugateErPresentTense(verb));
+    document.write(conjugateErPresentTense(verb));
 }
 else if (verbErTest(verb) && (tense == "future")) {
     verb = verb.substring(0, verb.length - 2);
-    alert(conjugateErFutureTense(verb));
+    document.write(conjugateErFutureTense(verb));
 }
 else if (verbIrTest(verb) && (tense == "past")) {
     verb = verb.substring(0, verb.length - 2);
-    alert(conjugateIrPastTense(verb));
+    document.write(conjugateIrPastTense(verb));
 }
 else if (verbIrTest(verb) && (tense == "present")) {
     verb = verb.substring(0, verb.length - 2);
-    alert(conjugateIrPresentTense(verb));
+    document.write(conjugateIrPresentTense(verb));
 }
 else if (verbIrTest(verb) && (tense == "future")) {
     verb = verb.substring(0, verb.length - 2);
-    alert(conjugateIrFutureTense(verb));
+    document.write(conjugateIrFutureTense(verb));
 }
 else if (verbArTest(verb) && (tense == "past")) {
     verb = verb.substring(0, verb.length - 2);
-    alert(conjugateArPastTense(verb));
+    document.write(conjugateArPastTense(verb));
 }
 else if (verbArTest(verb) && (tense == "present")) {
     verb = verb.substring(0, verb.length - 2);
-    alert(conjugateArPresentTense(verb));
+    document.write(conjugateArPresentTense(verb));
 }
 else if (verbArTest(verb) && (tense == "future")) {
     verb = verb.substring(0, verb.length - 2);
-    alert(conjugateArFutureTense(verb));
+   document.write(conjugateArFutureTense(verb));
 }
