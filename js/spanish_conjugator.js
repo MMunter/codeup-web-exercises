bc90d7c19313669f7f2c6f6a09e68509a0a7821c
mdm911 <mdm911@yahoo.com>
2018-02-07 00:08:36 -0600

    Added background image to tommy_munter_page and added center text- align to spanish_conjugator

--- js/spanish_conjugator.js
+++ js/spanish_conjugator.js
@@ -1,4 +1,5 @@
 document.getElementsByTagName("body")[0].style.backgroundColor = "red";
+document.getElementById("demo").style.textAlign = "center";
 
 //functions for all regular verb endings and tenses
 function conjugateErPresentTense (string) {
@@ -102,7 +103,6 @@ function verbArTest(string) {
 
      var tense = document.getElementById("tense").value;
      tense = tense.toLowerCase();
-
 
      if (verbErTest(verb) && (tense == "past")) {
          verb = verb.substring(0, verb.length - 2);
