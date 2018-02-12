2b5c432838a5e1a1879ef0defa8929dd1181ac50
mdm911 <mdm911@yahoo.com>
2018-02-12 14:26:01 -0600

    Completed jquery events exercise

--- js/jquery_exercises.js
+++ js/jquery_exercises.js
@@ -1,5 +1,18 @@
 "use strict";
 
 $("h1").click(function() {
-   $(this).css("background-color", "purple");
+    $(this).css("background-color", "blue");
 });
+
+$("p").dblclick(function() {
+    $(this).css("font-size", "18px");
+});
+
+$("li").hover(
+    function() {
+        $(this).css("color", "red");
+    },
+    function() {
+        $(this).css("color", "black");
+    }
+);
