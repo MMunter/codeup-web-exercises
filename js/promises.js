fc9861308e07edfb6ee10a7ab231a86be4868762
mdm911 <mdm911@yahoo.com>
2018-02-23 00:36:36 -0600

    In progress promises exercise, styling spanish_verb_conjugator

--- js/promises.js
+++ js/promises.js
@@ -1,25 +1,28 @@
-function wait(number) {
-    return new Promise((resolve) => {
+const wait = (number) => {
+    return new Promise((resolve,reject) => {
         setTimeout(resolve, number);
         });
-}
+};
 
 wait(1000).then(() => console.log("You'll see this after 1 second"));
 wait(3000).then(() => console.log("You'll see this after 3 seconds"));
 
-
-fetch("https://api.github.com/users/" + username + "/repos?per_page=100", {headers: {"Authorization": "token cc1562aa71d592d9a9fd63709f141e3b1450c7d6" }})
-    .then(response => response.json())
-    .then(repositories => {
-        repositories.map(repo => {
-           dates.push(repo.updated_at);
+function getDateOfLastCommit(username) {
+    let dates = [];
+    fetch("https:// api.github.com/users/" + username + "/events/public", {headers: {"Authorization": "token  6d3d97fce1297eef018efd3b270eacb723e83b93"}})
+        .then(response => response.json())
+        .then((repositories) => {
+            repositories.map(repo => {
+                dates.push(repo.created_at);
+            });
+            dates.sort();
+            document.querySelector("span").innerText = new Date(dates[dates.length - 1]);
+            console.log(dates);
         });
-        dates.sort();
-        document.querySelector("span").innerText = new Date(dates[dates.length -1]);
-        });
+}
 
 
-    getDateOfLastCommit("mdm911");
+getDateOfLastCommit("mdm911");
 
 
 
